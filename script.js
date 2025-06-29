b20b40f8d1b430a3c201ae5367f18c2591292455
bayualfisyahr <bayualfi07@gmail.com>
2025-06-29 10:46:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // KONFIGURASI PENTING - HARAP DIISI
 // =================================================================================
 // URL Web App BARU dari Google Apps Script yang sudah Anda deploy
-const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby97JTGueEOzTefroE_AmRT5hBgveAAi535whYbczS9LqYU0Uoth7SIx29AyUsNX57S/exec'; 
+const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzwY7ihSKIaUKTvzHzBBLWQTJx9OZ6HLfPnUgnSxO5uSBuN2qdRcBj9OrGvB33Unb1V/exec'; 
 
 // URL Webhook n8n Anda yang sudah berjalan
 const N8N_WEBHOOK_URL = 'https://bayualfi.app.n8n.cloud/webhook/15a69324-bbc0-4b25-82cb-2f9ef519b8ea';
